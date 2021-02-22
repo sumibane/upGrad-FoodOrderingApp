fe4190142d88f2da0bc3cc36bfbda6d66dbe6faf
Rashmi-incvrce <70973255+Rashmi-incvrce@users.noreply.github.com>
2021-02-22 19:27:30 +0530

    Update Checkout.js
    Please integrate with the main application

---
+++ src/screens/checkout/Checkout.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+//Router import for redirection.
+import {Route, Switch} from "react-router-dom";
+
+//Imports of different pages in the application
+import Home from './screens/home/Home';
+import Profile from './screens/profile/Profile';
+import Details from './screens/details/Details';
+import Checkout from './screens/checkout/Checkout';
+
+/**
+ * Food Ordering Application controller
+ */
+class FoodOrderingApp extends Component {
+    constructor() {
+        super();
+        this.baseUrl = 'http://localhost:8080/api/'
+    }
+
+    render() {
+        return (
+            //Routing accomplishment
+            <Switch>
+                <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
+                <Route exact path='/profile' render={(props) => <Profile {...props} />}/>
+                <Route exact path="/restaurant/:id" render={(props) => <Details {...props} baseUrl={this.baseUrl} />}/>
+                <Route path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
+            </Switch>
+        )
+    }
+}
+
+export default FoodOrderingApp;
