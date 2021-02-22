a139b5c971bde81e1ac2fa7a885b65005138f0a7
Sumit Banerjee <sum123.ban@gmail.com>
2021-02-22 14:42:16 +0530

    Routing added to the Cart

--- src/FoodOrderingApp.js
+++ src/FoodOrderingApp.js
@@ -6,6 +6,7 @@ import {Route, Switch} from "react-router-dom";
 //Imports of different pages in the application
 import Home from './screens/home/Home';
 import Profile from './screens/profile/Profile';
+import Details from './screens/details/Details';
 
 /**
  * Food Ordering Application controller
@@ -22,6 +23,7 @@ class FoodOrderingApp extends Component {
             <Switch>
                 <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
                 <Route exact path='/profile' render={(props) => <Profile {...props} />}/>
+                <Route exact path="/restaurant/:id" render={(props) => <Details {...props} baseUrl={this.baseUrl} />}/>
             </Switch>
         )
     }
